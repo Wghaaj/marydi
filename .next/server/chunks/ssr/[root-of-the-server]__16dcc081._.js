6ce38fe2cd6604b678183d0e6bed0b94d6057e21
wghaaj <dianatatarenko160@gmail.com>
2025-11-09 10:31:16 +0000

    changed the image path

--- .next/server/chunks/ssr/[root-of-the-server]__16dcc081._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[93695,(a,b,c)=>{b.exports=a.x("next/dist/shared/lib/no-fallback-error.external.js",()=>require("next/dist/shared/lib/no-fallback-error.external.js"))},62212,a=>{a.n(a.i(66114))},49901,(a,b,c)=>{},50708,a=>{"use strict";a.s(["default",()=>d]);var b=a.i(7997),c=a.i(20811);function d(){return(0,b.jsx)(c.default,{})}}];
-
-//# sourceMappingURL=%5Broot-of-the-server%5D__16dcc081._.js.map
